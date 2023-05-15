0a586b6c7bad6172e69faa199eb1e76d6bd614af
James Charlesworth <james.charlesworth@gmail.com>
2023-05-15 08:56:24 -0400

    adds logger

--- src/common/store/index.ts
+++ src/common/store/index.ts
@@ -1,10 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 import counterReducer from './reducers/counterSlice';
+
+const logger = createLogger({
+  collapsed: true,
+})
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+    logger
+  ),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
